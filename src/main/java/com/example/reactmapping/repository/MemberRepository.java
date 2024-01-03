d3a52c02135d91cb2bf9f4dd746f65644509462f
JungHoon Kim <kik1232198@gmail.com>
2024-01-03 21:24:27 +0900

    .

---
+++ src/main/java/com/example/reactmapping/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.reactmapping.repository;
+
+import com.example.reactmapping.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member>findMemberByEmailId(String emailId);
+}

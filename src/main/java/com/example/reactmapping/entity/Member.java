ce71a2138d1cd7ef962519bc40bf34942fe656b8
JungHoon Kim <kik1232198@gmail.com>
2024-01-03 21:23:54 +0900

    .

---
+++ src/main/java/com/example/reactmapping/entity/Member.java
@@ -0,0 +1,33 @@
+package com.example.reactmapping.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Schema(description = "회원 이메일")
+    private String emailId;
+    @Schema(description = "회원 비밀번호")
+    private String password;
+    @Schema(description = "회원 이름")
+    private String username;
+    @Schema(description = "권한")
+    private String role;
+
+
+}

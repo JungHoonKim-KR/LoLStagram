dca6188aa3214d9bfb94a532170a2e31eac8b59b
JungHoon Kim <kik1232198@gmail.com>
2024-01-04 21:36:14 +0900

    .

--- src/main/java/com/example/reactmapping/dto/LoginRequestDto.java
+++ src/main/java/com/example/reactmapping/dto/LoginRequestDto.java
@@ -2,10 +2,12 @@ package com.example.reactmapping.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
-public class LoginRequest {
+public class LoginRequestDto {
     private String emailId;
     private String password;
 }

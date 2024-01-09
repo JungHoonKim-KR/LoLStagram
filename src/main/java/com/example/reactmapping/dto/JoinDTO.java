3596a6417dce4e75dc38716436f81b78844ea6ea
JungHoon Kim <kik1232198@gmail.com>
2024-01-09 22:33:01 +0900

    .

---
+++ src/main/java/com/example/reactmapping/dto/JoinDTO.java
@@ -0,0 +1,14 @@
+package com.example.reactmapping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinDTO {
+    private String emailId;
+    private String password;
+    private String username;
+}

bc451dfa00b2b9e6a2fa6c9dc8420f28ed958ac2
JungHoon Kim <kik1232198@gmail.com>
2024-01-09 21:27:18 +0900

    .

---
+++ src/main/java/com/example/reactmapping/entity/AccessToken.java
@@ -0,0 +1,33 @@
+package com.example.reactmapping.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class AccessToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String emailId;
+    @NotBlank
+    private String accessToken;
+
+
+    public AccessToken(String emailId, String accessToken) {
+        this.emailId = emailId;
+        this.accessToken = accessToken;
+    }
+
+    public AccessToken updateToken(String token) {
+        this.accessToken = token;
+        return this;
+    }
+}

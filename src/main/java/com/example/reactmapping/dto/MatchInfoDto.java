b065027a24089ed57ac706804b2aa033d049b72c
JungHoon Kim <kik1232198@gmail.com>
2024-02-02 21:57:36 +0900

    MatchInfo

---
+++ src/main/java/com/example/reactmapping/dto/MatchInfoDto.java
@@ -0,0 +1,69 @@
+package com.example.reactmapping.dto;
+
+import com.example.reactmapping.StringListConverter;
+import com.example.reactmapping.entity.MatchInfo;
+import jakarta.persistence.Convert;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder(toBuilder = true)
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MatchInfoDto {
+    private String matchId;
+    private Long kills;
+    private Long deaths;
+    private Long assists;
+    private String kda;
+    private String championName;
+    private Long mainRune;
+    private Long subRune;
+    @Convert(converter = StringListConverter.class)
+    private List<Integer> itemList = new ArrayList<>();
+    @Convert(converter = StringListConverter.class)
+    private List<Integer> summonerSpellList = new ArrayList<>();
+    private String result;
+
+    public static List<MatchInfoDto> entityToDto(List<MatchInfo> matchInfoList){
+        return matchInfoList.stream()
+                .map(matchInfo -> {
+                            MatchInfoDto build = MatchInfoDto.builder()
+                                    .matchId(matchInfo.getMatchId())
+                                    .kills(matchInfo.getKills())
+                                    .deaths(matchInfo.getDeaths())
+                                    .assists(matchInfo.getAssists())
+                                    .kda(matchInfo.getKda())
+                                    .championName(matchInfo.getChampionName())
+                                    .mainRune(matchInfo.getMainRune())
+                                    .subRune(matchInfo.getSubRune())
+                                    .result(matchInfo.getResult())
+                                    .build();
+                            return build.toBuilder().itemList(matchInfo.getItemList()).summonerSpellList(matchInfo.getSummonerSpellList()).build();
+                        }
+                ).collect(Collectors.toList());
+    }
+
+    public static MatchInfo dtoToEntity(MatchInfoDto matchInfoDto){
+        return MatchInfo.builder()
+                .matchId(matchInfoDto.getMatchId())
+                .kills(matchInfoDto.getKills())
+                .deaths(matchInfoDto.getDeaths())
+                .assists(matchInfoDto.getAssists())
+                .kda(matchInfoDto.getKda())
+                .championName(matchInfoDto.getChampionName())
+                .mainRune(matchInfoDto.getMainRune())
+                .subRune(matchInfoDto.getSubRune())
+                .itemList(matchInfoDto.getItemList())
+                .summonerSpellList(matchInfoDto.getSummonerSpellList())
+                .result(matchInfoDto.getResult())
+                .build();
+    }
+}
---
+++ src/main/java/com/example/reactmapping/entity/MatchInfo.java
@@ -0,0 +1,60 @@
+package com.example.reactmapping.entity;
+
+import com.example.reactmapping.StringListConverter;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class MatchInfo implements Persistable<String> {
+    @Id
+    private String matchId;
+    private Long gameStartTimestamp;
+    private Long kills;
+    private Long deaths;
+    private Long assists;
+    private String kda;
+    private String championName;
+    private Long mainRune;
+    private Long subRune;
+    @Convert(converter = StringListConverter.class)
+    private List<Integer> itemList = new ArrayList<>();
+    @Convert(converter = StringListConverter.class)
+    private List<Integer> summonerSpellList = new ArrayList<>();
+    private String result;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "summoner_id")
+    @JsonBackReference
+    private SummonerInfo SummonerInfo;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createTime;
+    @PrePersist
+    protected void onCreate() {
+        createTime = LocalDateTime.now();
+    }
+    @Override
+    public boolean isNew() {
+        return createTime == null;
+    }
+
+    // ID 필드에 대한 getter
+    @Override
+    public String getId() {
+        return matchId;
+    }
+
+}

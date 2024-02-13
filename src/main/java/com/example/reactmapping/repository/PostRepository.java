dacd4cc1e90a212240b6a4ca99eab19f0343e7fc
JungHoon Kim <kik1232198@gmail.com>
2024-02-13 13:22:15 +0900

    LoLService

---
+++ src/main/java/com/example/reactmapping/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.example.reactmapping.repository;
+
+import com.example.reactmapping.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAll();
+}

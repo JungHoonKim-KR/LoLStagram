41231c34a0bf18eb1db62ba55ee249a86d0d4473
JungHoon Kim <kik1232198@gmail.com>
2024-02-14 20:06:00 +0900

    LoginResponseDto

---
+++ src/main/java/com/example/reactmapping/dto/LoginResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.reactmapping.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+    private String username;
+    private SummonerInfoDto summonerInfoDto;
+    private MemberDto memberDto;
+    //승률, 전적 추가
+}

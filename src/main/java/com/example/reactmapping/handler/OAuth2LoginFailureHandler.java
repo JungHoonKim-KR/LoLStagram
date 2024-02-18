1aeedee4eff577bd154c95428b3e11f07fb0d5d1
JungHoon Kim <kik1232198@gmail.com>
2024-02-18 20:59:09 +0900

    OAuth2

---
+++ src/main/java/com/example/reactmapping/handler/OAuth2LoginFailureHandler.java
@@ -0,0 +1,19 @@
+package com.example.reactmapping.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Slf4j
+@Component
+public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.info("로그인 실패");
+    }
+}

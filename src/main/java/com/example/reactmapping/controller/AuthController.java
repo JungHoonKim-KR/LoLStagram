3257520c8fd3d5ba2b3af351c85962b94947daf2
JungHoon Kim <kik1232198@gmail.com>
2024-02-22 21:19:44 +0900

    프로필 수정 추가

--- src/main/java/com/example/reactmapping/controller/AuthController.java
+++ src/main/java/com/example/reactmapping/controller/AuthController.java
@@ -1,14 +1,16 @@
 package com.example.reactmapping.controller;
-
-import com.example.reactmapping.dto.AuthenticationDto;
-import com.example.reactmapping.dto.JoinDTO;
-import com.example.reactmapping.dto.LoginRequestDto;
-import com.example.reactmapping.dto.LoginResponseDto;
+import com.example.reactmapping.dto.*;
+import com.example.reactmapping.entity.MatchInfo;
 import com.example.reactmapping.entity.Member;
 import com.example.reactmapping.exception.AppException;
 import com.example.reactmapping.exception.ErrorCode;
+import com.example.reactmapping.repository.MatchRepository;
 import com.example.reactmapping.service.AuthService;
-
+import com.example.reactmapping.service.LoLService;
+import com.example.reactmapping.service.LogoutService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,37 +18,48 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final LogoutService logoutService;
     @Operation(summary = "회원가입", description = "새로운 회원 등록")
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody @Parameter(name = "변수", description = "회원 이메일, 비밀번호, 이름") JoinDTO dto) {
+    public ResponseEntity<?> join(@RequestPart("joinDto") @Parameter(name = "변수", description = "회원 이메일, 비밀번호, 이름") JoinDTO dto
+                                    , @RequestPart("image")MultipartFile image) throws IOException {
+        dto.setImg(image);
         Member member = authService.join(dto);
         return ResponseEntity.ok().body(member);
-
     }
 
     @Operation(summary = "로그인")
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto dto, HttpServletResponse httpServletResponse) {
-        LoginResponseDto responseDto = authService.login(httpServletResponse,dto.getEmailId(),dto.getPassword(),dto.getAuthenticationCode());
+    public ResponseEntity<LoginResponseDto> login(HttpSession httpSession,@RequestBody LoginRequestDto dto, HttpServletResponse httpServletResponse) throws JsonProcessingException {
+        LoginResponseDto responseDto = authService.login(httpSession,httpServletResponse,dto.getEmailId(),dto.getPassword(),dto.getAuthenticationCode());
         return ResponseEntity.ok().body(responseDto);
     }
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/oauthLogin")
-    public ResponseEntity<?> oauthLogin(@RequestBody LoginRequestDto dto, HttpSession session, HttpServletResponse httpServletResponse){
+    public ResponseEntity<?> oauthLogin(HttpSession httpSession,@RequestBody LoginRequestDto dto, HttpSession session, HttpServletResponse httpServletResponse) throws JsonProcessingException {
         AuthenticationDto authenticationDto = (AuthenticationDto) session.getAttribute("AuthenticationDto");
         if(authenticationDto.getCode().equals(dto.getAuthenticationCode())){
-            LoginResponseDto responseDto = authService.login(httpServletResponse,authenticationDto.getEmailId(),dto.getPassword(),dto.getAuthenticationCode());
-            log.info(String.valueOf(responseDto));
+            LoginResponseDto responseDto = authService.login(httpSession,httpServletResponse,authenticationDto.getEmailId(),dto.getPassword(),dto.getAuthenticationCode());
             return ResponseEntity.ok().body(responseDto);
         }
         else {
@@ -54,17 +67,12 @@ public class AuthController {
             return new ResponseEntity<>(exception, HttpStatus.UNAUTHORIZED);
         }
     }
+
     @ResponseBody
     @PostMapping("/logout")
-    public String logout(HttpServletRequest request) {
-        return "로그아웃";
+    public void logout() {
+
     }
 
-
-    @GetMapping("/oauth2/login/google")
-    public String test() {
-
-        return "완료";
-    }
 
 }

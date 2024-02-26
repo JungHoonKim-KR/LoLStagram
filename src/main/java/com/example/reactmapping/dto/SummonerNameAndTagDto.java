ac89153f6bb186a9235ec855e1ea8a05e5c8fa41
JungHoon Kim <kik1232198@gmail.com>
2024-02-26 17:22:59 +0900

    함수 반환 Dto

---
+++ src/main/java/com/example/reactmapping/dto/SummonerNameAndTagDto.java
@@ -0,0 +1,13 @@
+package com.example.reactmapping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SummonerNameAndTagDto {
+    private String summonerName;
+    private String summonerTag;
+}

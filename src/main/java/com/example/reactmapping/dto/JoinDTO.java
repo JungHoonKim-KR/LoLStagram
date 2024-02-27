99095424d2b68aff16c1ea0d32d093957394ed5a
JungHoon Kim <kik1232198@gmail.com>
2024-02-27 15:55:21 +0900

    naming update

--- src/main/java/com/example/reactmapping/dto/JoinDTO.java
+++ src/main/java/com/example/reactmapping/dto/JoinDTO.java
@@ -1,10 +1,12 @@
 package com.example.reactmapping.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class JoinDTO {
@@ -13,5 +15,6 @@ public class JoinDTO {
     private String username;
     private String summonerName;
     private String summonerTag;
+    private MultipartFile img;
 
 }

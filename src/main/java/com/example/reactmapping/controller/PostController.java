35ffb77e9955bd9c21e82d75d3b624ac901db393
JungHoon Kim <kik1232198@gmail.com>
2024-02-29 19:14:07 +0900

    이름 변경

---
+++ src/main/java/com/example/reactmapping/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.example.reactmapping.controller;
+
+import com.example.reactmapping.dto.PostDto;
+import com.example.reactmapping.dto.PostResultDto;
+import com.example.reactmapping.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping("/write")
+    public void write(@RequestPart("postDto") PostDto postDto, @RequestPart(name = "image", required = false)MultipartFile image) throws IOException {
+        if(image!=null)
+            postDto = postDto.toBuilder().serverImg(image).build();
+        postService.save(postDto);
+    }
+
+    @GetMapping("/postList")
+    public PostResultDto getPostList(@PageableDefault(size = 3,direction = Sort.Direction.DESC)  Pageable pageable) {
+        return postService.getPostList(pageable);
+    }
+}

b0b59e75aabc59efe1eac8807066ad648069358c
JungHoon Kim <kik1232198@gmail.com>
2024-03-09 21:13:38 +0900

    예외 처리

--- src/main/java/com/example/reactmapping/exception/ExceptionManager.java
+++ src/main/java/com/example/reactmapping/exception/ExceptionManager.java
@@ -4,10 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class ExceptionManager {
     @ExceptionHandler(AppException.class)
     public ResponseEntity<?> appExceptionHandler(AppException e){
-        return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(e.getErrorCode().name()+" "+e.getMessage());
+        Map<String,Object> body = new HashMap<>();
+        body.put("errorCode", e.getErrorCode().name());
+        body.put("errorMessage", e.getMessage());
+        return new ResponseEntity<>(body,e.getErrorCode().getHttpStatus());
     }
 }

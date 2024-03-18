cbd0cfb47b0922b7d751747cccc148bc000bcad8
JungHoon Kim <kik1232198@gmail.com>
2024-03-18 18:09:46 +0900

    .

---
+++ src/main/java/com/example/reactmapping/dto/MatchInfoResultDto.java
@@ -0,0 +1,16 @@
+package com.example.reactmapping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class MatchInfoResultDto {
+    public List<MatchInfoDto> matchInfoDtoList;
+    public Boolean isLast;
+    public String type;
+}

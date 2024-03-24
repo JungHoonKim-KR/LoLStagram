4edbc6f5270dbb08c20e7bc3ae8b9760b8bf746a
JungHoon Kim <kik1232198@gmail.com>
2024-03-24 18:57:40 +0900

    .

--- src/main/java/com/example/reactmapping/repository/MatchRepository.java
+++ src/main/java/com/example/reactmapping/repository/MatchRepository.java
@@ -1,20 +1,28 @@
 package com.example.reactmapping.repository;
 
 import com.example.reactmapping.entity.MatchInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MatchRepository extends JpaRepository<MatchInfo,String> {
-    @Query("select m from MatchInfo m where m.SummonerInfo.summonerId =:summonerId order by m.gameStartTimestamp DESC ")
+    @Query("select m from MatchInfo m where m.summonerInfo.summonerId =:summonerId order by m.gameStartTimestamp DESC ")
     List<MatchInfo> findAllBySummonerId(@Param("summonerId")String summonerId);
+    Page<MatchInfo> findAll(Specification<MatchInfo> spec, Pageable pageable);
+
 
     @Modifying
-    @Query("UPDATE MatchInfo s SET " +
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Query("update MatchInfo s SET " +
             "s.matchId = :matchId, " +
             "s.gameStartTimestamp = :gameStartTimestamp, " +
             "s.kills = :kills, " +

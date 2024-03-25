85688e100ffe7a771374c69b1404203ca1de95f5
JungHoon Kim <kik1232198@gmail.com>
2024-03-25 19:24:15 +0900

    .

--- src/main/java/com/example/reactmapping/entity/Member.java
+++ src/main/java/com/example/reactmapping/entity/Member.java
@@ -1,11 +1,9 @@
 package com.example.reactmapping.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.util.ArrayList;
 import java.util.List;

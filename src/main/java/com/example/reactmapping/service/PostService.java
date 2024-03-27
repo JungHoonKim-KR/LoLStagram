8a9341785453ad4fd198ebdf35b40b146a6c896f
JungHoon Kim <kik1232198@gmail.com>
2024-03-27 15:54:48 +0900

    .

--- src/main/java/com/example/reactmapping/service/PostService.java
+++ src/main/java/com/example/reactmapping/service/PostService.java
@@ -1,12 +1,20 @@
 package com.example.reactmapping.service;
 
 import com.example.reactmapping.dto.PostDto;
+import com.example.reactmapping.dto.PostResultDto;
+import com.example.reactmapping.entity.Image;
 import com.example.reactmapping.entity.Member;
 import com.example.reactmapping.entity.Post;
+import com.example.reactmapping.norm.ImageType;
 import com.example.reactmapping.repository.MemberRepository;
 import com.example.reactmapping.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,46 +28,43 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
+    private final ImgService imgService;
+
     public void save(PostDto postDto) throws IOException {
         Member findMember = memberRepository.findMemberById(postDto.getMemberId()).get();
-        String img = null;
-//        if (postDto.getImg() != null) {
-//            img = createImg(postDto.getImg());
-//        }
+        Image img ;
         Post post = Post.builder()
                 .title(postDto.getTitle())
                 .content(postDto.getContent())
-//                .img(img)
                 .member(findMember)
                 .createTime(LocalDateTime.now())
                 .build();
+        if (postDto.getServerImg() != null) {
+            img = imgService.createImg(postDto.getServerImg(),null,postDto.getMemberId(), String.valueOf(ImageType.PostType));
+            post = post.toBuilder().image(img).build();
+        }
         postRepository.save(post);
     }
-    public List<PostDto> getPostList(){
-        List<Post> findPostList = postRepository.findAll();
+    public PostResultDto getPostList(Pageable pageable){
+        log.info(String.valueOf(pageable.getPageNumber()));
+        log.info(String.valueOf(pageable.getPageSize()));
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.DESC, "createTime"));
+        Page<Post> findPostObject = postRepository.findAll(pageRequest);
+        List<Post> postList = findPostObject.getContent();
         List<PostDto>postDtoList= new ArrayList<>();
-        for(Post post : findPostList){
+        for(Post post : postList){
             PostDto postDto = PostDto.entityToDto(post);
             postDtoList.add(postDto);
         }
-        return postDtoList;
+
+        return new PostResultDto(postDtoList,findPostObject.isLast());
     }
 
-    public String createImg(MultipartFile file) throws IOException {
-        if(file.getOriginalFilename()==""){
-            return null;
-        }
-        String originalFilename = file.getOriginalFilename();
-        String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."), originalFilename.length());
-        String uuid = String.valueOf(UUID.randomUUID());
 
-        InputStream inputStream = file.getInputStream();
-        File tempFile = File.createTempFile(uuid, fileExtension);
-        FileUtils.copyInputStreamToFile(inputStream, tempFile);
-        //// naverCloudServer 사용 예정
-        return null;
-    }
+
+
 }

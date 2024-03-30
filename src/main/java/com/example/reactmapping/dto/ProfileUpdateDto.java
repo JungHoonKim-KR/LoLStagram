8574ac6ca9a8428c54a79aa8ffb2757287cae5f9
JungHoon Kim <kik1232198@gmail.com>
2024-03-30 17:13:58 +0900

    .

--- src/main/java/com/example/reactmapping/dto/ProfileUpdateDto.java
+++ src/main/java/com/example/reactmapping/dto/ProfileUpdateDto.java
@@ -1,15 +1,18 @@
 package com.example.reactmapping.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
-@Getter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileUpdateDto {
     private Long id;
     private String summonerName;
     private String summonerTag;
+    private MultipartFile img;
 
 }

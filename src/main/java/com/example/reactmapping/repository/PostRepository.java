c63630d3df9dc2fcc39f0dc1ec693e820ea774ae
JungHoon Kim <kik1232198@gmail.com>
2024-03-31 23:16:34 +0900

    .

--- src/main/java/com/example/reactmapping/repository/PostRepository.java
+++ src/main/java/com/example/reactmapping/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.example.reactmapping.repository;
 
 import com.example.reactmapping.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAll();
+
+    @Override
+    Page<Post> findAll(Pageable pageable);
 }

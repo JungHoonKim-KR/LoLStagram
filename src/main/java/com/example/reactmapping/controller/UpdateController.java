78f4b20462fa3f1413e95041009b2e3e554da4ab
JungHoon Kim <kik1232198@gmail.com>
2024-04-04 15:56:41 +0900

    .

--- src/main/java/com/example/reactmapping/controller/UpdateController.java
+++ src/main/java/com/example/reactmapping/controller/UpdateController.java
@@ -1,20 +1,17 @@
 package com.example.reactmapping.controller;
 
-import com.example.reactmapping.dto.CallSummonerInfoResponse;
-import com.example.reactmapping.dto.ProfileUpdateDto;
-import com.example.reactmapping.dto.SummonerInfoDto;
-import com.example.reactmapping.dto.SummonerNameAndTagDto;
+import com.example.reactmapping.dto.*;
 import com.example.reactmapping.service.AuthService;
-import com.example.reactmapping.service.LoLService;
+import com.example.reactmapping.service.MatchService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -22,6 +19,7 @@ import java.util.Map;
 //@CrossOrigin(origins = "http://localhost:3000", exposedHeaders = "Authorization")
 public class UpdateController {
     private final AuthService authService;
+    private final MatchService matchService;
     @PostMapping("/update")
     public SummonerInfoDto update(@RequestBody SummonerNameAndTagDto summonerNameAndTagDto) throws JsonProcessingException {
         CallSummonerInfoResponse callSummonerInfoResponse = authService.callSummonerInfo(summonerNameAndTagDto.getSummonerName(), summonerNameAndTagDto.getSummonerTag());
@@ -29,8 +27,15 @@ public class UpdateController {
     }
 
     @PostMapping("/profileUpdate")
-    public void profileUpdate(@RequestBody ProfileUpdateDto profileUpdateDto) throws JsonProcessingException {
+    public void profileUpdate(@RequestPart("profileDto") ProfileUpdateDto profileUpdateDto
+                               , @RequestPart("image")MultipartFile img) throws IOException {
+        profileUpdateDto.setImg(img);
         authService.updateProfile(profileUpdateDto);
+    }
+
+    @PostMapping("/matchUpdate")
+    public MatchInfoResultDto matchUpdate(@RequestBody MatchInfoRequestDto matchInfoRequestDto, @PageableDefault(size =10) Pageable pageable){
+        return matchService.getMatchList(pageable,matchInfoRequestDto.getType(), matchInfoRequestDto.getSummonerId());
     }
 }
 

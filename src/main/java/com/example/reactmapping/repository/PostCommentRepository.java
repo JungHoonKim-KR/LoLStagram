be3aea1412c00f2f1d09b503f1aa47414c57a614
JungHoon Kim <kik1232198@gmail.com>
2024-05-19 23:35:50 +0900

    PostComment 생성

---
+++ src/main/java/com/example/reactmapping/repository/PostCommentRepository.java
@@ -0,0 +1,8 @@
+package com.example.reactmapping.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestPart;
+
+@Repository
+public interface PostCommentRepository {
+}

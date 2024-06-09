4225a18355094eca03963a60efbdd956b65a546e
JungHoonKim-KR <kik1232198@gmail.com>
2024-06-09 13:48:41 +0900

    LOLService 경로 변경

--- src/main/java/com/example/reactmapping/service/AuthService.java
+++ src/main/java/com/example/reactmapping/service/AuthService.java
@@ -12,6 +12,7 @@ import com.example.reactmapping.entity.Image;
 import com.example.reactmapping.object.MostChampion;
 import com.example.reactmapping.object.RefreshToken;
 import com.example.reactmapping.repository.*;
+import com.example.reactmapping.service.LoL.LoLService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.Cookie;
@@ -25,7 +26,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,6 +46,7 @@ public class AuthService {
     private final EntityManager entityManager;
 
     public Member join(JoinDTO dto) throws IOException {
+        // 회원 아이디가 이미 존재하는지
         if (memberRepository.findMemberByEmailId(dto.getEmailId()).isPresent()) {
             throw new AppException(ErrorCode.DUPLICATED, dto.getEmailId() + "는 이미 존재합니다.");
         }
@@ -68,7 +69,9 @@ public class AuthService {
         }
         else {
             memberRepository.save(member);
+
         }
+
         return member;
     }
 
@@ -83,6 +86,7 @@ public class AuthService {
                 if (!bCryptPasswordEncoder.matches(requestPassword, member.getPassword())) {
                     throw new AppException(ErrorCode.ACCESS_ERROR, "비밀번호가 일치하지 않습니다.");
                 }
+
             }
         } else throw new AppException(ErrorCode.NOTFOUND, requestEmail + "는 존재하지 않습니다.");
         //토큰 발급
@@ -104,6 +108,7 @@ public class AuthService {
         refreshToken = new RefreshToken(tokenDto.getUserEmail(), tokenDto.getRefreshToken());
     }
         refreshTokenRepository.save(refreshToken);
+
         SummonerInfo summonerInfo = summonerInfoRepository.findBySummonerId(member.getSummonerId()).get();
         //Get MatchList
         List<MatchInfoDto> matchList = matchService.getMatchList(pageable, type,summonerInfo.getSummonerId()).getMatchInfoDtoList();
@@ -121,6 +126,7 @@ public class AuthService {
         return loginResponseDto;
 
     }
+
     //응답에 access 토큰 부여
     //이건 헤더에 직접적으로 토큰을 부여하는게 아니라 프론트엔드에게 전달할 응답값에 토큰을 넣는 것임.
     //request : 클라이언트의 요청값, response: 백 -> 프론트 전달값
@@ -157,21 +163,23 @@ public class AuthService {
 
                 // 중첩된 개수를 구한다.
                 int newGameCount = LOL.INFO.getGameCount() - compare.getResult();
-
                 for(int i = LOL.INFO.getGameCount()-1, j = 0; j<newGameCount;i--, j++){
                     MatchInfo matchInfo = matchInfoList.get(j);
+                    //originMatchList 정보를 일부 활용하기 때문에 예외적으로 Repo에 직접 접근 허용
                     matchRepository.updateAll(matchInfo.getMatchId(),matchInfo.getGameStartTimestamp(),matchInfo.getKills()
                             ,matchInfo.getDeaths(),matchInfo.getAssists(),matchInfo.getKda(),matchInfo.getChampionName(),matchInfo.getMainRune(),matchInfo.getSubRune()
                             ,matchInfo.getItemList(),matchInfo.getSummonerSpellList(),matchInfo.getResult()
                     ,originMatchList.get(i).getMatchId());
                 }
             }
+            //mostchampion 설정
             List<MostChampion> mostChampionList = loLService.calcMostChampion(matchInfoList);
             summonerProfile = summonerProfile.toBuilder().mostChampionList(mostChampionList).build();
 
             for (MatchInfo matchInfo : matchInfoList) {
                 summonerProfile.addMatchInfo(matchInfo);
             }
+
             // 바로 저장
             if(compare.getResult()==-1){
                 // match save -> dirty checking : summonerInfo save
@@ -183,14 +191,17 @@ public class AuthService {
                 // 그래서 summonerInfo를 update하여 match update를 JPA에게 맡김
                 // 영속성 컨텍스트에 저장된 summonerInfo 객체를 추적하기 어려워 직접 찾아서 사용
                 SummonerInfo existingSummonerInfo = entityManager.find(SummonerInfo.class, summonerProfile.getId());
+
                 if (existingSummonerInfo != null) {
                     existingSummonerInfo.update(summonerProfile);
+
                     // 명시적으로 병합하여 변경 사항을 반영
                     entityManager.merge(existingSummonerInfo);
                 } else {
                     log.warn("SummonerInfo is not in the persistence context.");
                     throw new AppException(ErrorCode.NOTFOUND,"SummonerInfo is not in the persistence context.");
                 }
+
             }
         }
         return new CallSummonerInfoResponse(summonerProfile,summonerId);

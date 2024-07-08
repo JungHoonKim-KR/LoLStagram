7d7382705435aa2716708055147db5c1c2204f06
JungHoonKim-KR <kik1232198@gmail.com>
2024-07-08 21:44:15 +0900

    Image entity 재삭제

--- src/main/java/com/example/reactmapping/domain/Image/domain/Image.java
+++ src/main/java/com/example/reactmapping/domain/Image/domain/Image.java
@@ -1,18 +1,18 @@
-package com.example.reactmapping.domain.Image.domain;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+//package com.example.reactmapping.domain.Image.domain;
 //
-//@MappedSuperclass
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder(toBuilder = true)
-public class Image {
-    private String filename;
-    private String fileOriginName;
-    private String fileExtension;
-    private String fileUrl;
-}
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+////
+////@MappedSuperclass
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder(toBuilder = true)
+//public class Image {
+//    private String filename;
+//    private String fileOriginName;
+//    private String fileExtension;
+//    private String fileUrl;
+//}

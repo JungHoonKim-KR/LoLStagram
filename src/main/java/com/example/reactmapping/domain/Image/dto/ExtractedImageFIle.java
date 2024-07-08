27a8d6ffb6f52f16a3603f1eca13d90645adc13e
JungHoonKim-KR <kik1232198@gmail.com>
2024-07-08 21:36:01 +0900

    Image 리펙토링 1. Image entity 삭제 2. imageService 리펙토링

--- src/main/java/com/example/reactmapping/domain/Image/dto/ExtractedImageFIle.java
+++ src/main/java/com/example/reactmapping/domain/Image/dto/ExtractedImageFIle.java
@@ -1,18 +1,14 @@
-package com.example.reactmapping.domain.Image.domain;
+package com.example.reactmapping.domain.Image.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-//@MappedSuperclass
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder(toBuilder = true)
-public class Image {
+@NoArgsConstructor
+public class ExtractedImageFIle {
     private String filename;
     private String fileOriginName;
     private String fileExtension;
-    private String fileUrl;
 }

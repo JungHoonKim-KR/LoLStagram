d9958d2bf4ec33182aebf7ec31ef18ab5e0c1e0b
JungHoonKim-KR <kik1232198@gmail.com>
2024-07-19 11:24:49 +0900

    검색 Service 추가 및 수정

--- src/main/java/com/example/reactmapping/domain/lol/summonerInfo/repository/SummonerInfoRepository.java
+++ src/main/java/com/example/reactmapping/domain/lol/summonerInfo/repository/SummonerInfoRepository.java
@@ -1,21 +1,13 @@
 package com.example.reactmapping.domain.lol.summonerInfo.repository;
 
 import com.example.reactmapping.domain.lol.summonerInfo.domain.SummonerInfo;
-import com.example.reactmapping.domain.lol.dto.MostChampion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SummonerInfoRepository extends JpaRepository<SummonerInfo,String> {
-
     Optional<SummonerInfo> findBySummonerId(String summonerId);
+    Optional<SummonerInfo> findBySummonerNameAndSummonerTag(String summonerName, String summonerTag);
 
 }

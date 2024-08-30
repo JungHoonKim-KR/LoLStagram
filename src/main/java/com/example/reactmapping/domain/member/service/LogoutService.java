a7e76ca7de2fc2de2e81624087de492dde9abc8f
JungHoonKim-KR <kik1232198@gmail.com>
2024-08-30 22:16:55 +0900

    로그아웃 처리를 컨트롤러로 이전

--- src/main/java/com/example/reactmapping/domain/member/service/LogoutService.java
+++ src/main/java/com/example/reactmapping/domain/member/service/LogoutService.java
@@ -1,14 +1,9 @@
 package com.example.reactmapping.domain.member.service;
 
 import com.example.reactmapping.global.norm.Token;
-import com.example.reactmapping.global.security.jwt.RefreshTokenRepository;
-import com.example.reactmapping.global.security.cookie.CookieUtil;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.example.reactmapping.global.security.jwt.TokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,17 +13,20 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 @Transactional
-public class LogoutService implements LogoutHandler {
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final CookieUtil cookieUtil;
-    @Override
-    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        String refreshToken = cookieUtil.getCookieValue(request,Token.TokenName.refreshToken);
-        Optional<String> refreshTokenObject = refreshTokenRepository.findByRefreshToken(refreshToken);
+public class LogoutService {
+    private final TokenRepository tokenRepository;
+
+    public void logout(String accessToken, String refreshToken) {
+
+        //refreshToken 검증 후 삭제
+        Optional<String> refreshTokenObject = tokenRepository.findToken(refreshToken,Token.TokenType.REFRESH.name());
         if(refreshTokenObject.isPresent()){
-            refreshTokenRepository.delete(refreshToken);
+            tokenRepository.delete(refreshToken);
         }
-        request.getSession().invalidate();
+
+        //accessToken 블랙리스트에 등록
+        tokenRepository.registerBlacklist(accessToken);
+
         log.info("로그아웃");
     }
 }

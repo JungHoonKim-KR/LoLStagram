4d323e10c7e65171f6dfb98fa52e95cab850bbfc
JungHoonKim-KR <kik1232198@gmail.com>
2024-08-30 22:16:55 +0900

    로그아웃 처리를 컨트롤러로 이전

--- src/main/java/com/example/reactmapping/domain/member/controller/LogoutController.java
+++ src/main/java/com/example/reactmapping/domain/member/controller/LogoutController.java
@@ -1,13 +1,25 @@
 package com.example.reactmapping.domain.member.controller;
 
+import com.example.reactmapping.domain.member.service.LogoutService;
+import com.example.reactmapping.global.norm.Token;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
+@RequiredArgsConstructor
 public class LogoutController {
-    @ResponseBody
+    private final LogoutService logoutService;
+
     @PostMapping("/logout")
-    public void logout() {
+    public ResponseEntity<String> logout(HttpServletRequest httpServletRequest, @CookieValue(name = Token.TokenName.accessToken) String accessToken,
+                                       @CookieValue(name = Token.TokenName.refreshToken) String refreshToken) {
+
+        logoutService.logout(accessToken, refreshToken);
+        httpServletRequest.getSession().invalidate();
+        return ResponseEntity.ok("logout");
     }
 }

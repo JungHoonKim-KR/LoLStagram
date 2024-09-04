d51f8e780a18112630608eb50b8417ccf5cc8448
JungHoonKim-KR <kik1232198@gmail.com>
2024-09-04 23:56:21 +0900

    TestCode

---
+++ src/test/java/com/example/reactmapping/domain/member/controller/LogoutControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.reactmapping.domain.member.controller;
+
+import com.example.reactmapping.domain.member.service.LogoutService;
+import com.example.reactmapping.global.norm.Token;
+import jakarta.servlet.http.Cookie;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = LogoutController.class)
+class LogoutControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LogoutService logoutService;
+
+    @Test
+    void logoutTest() throws Exception {
+        //Given
+        String accessToken = "mocked-access-token";
+        String refreshToken = "mocked-refresh-token";
+
+        //when&then
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/logout")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + accessToken)
+                .cookie(new Cookie(Token.TokenName.refreshToken, refreshToken)))
+                .andExpect(status().isOk());
+
+        verify(logoutService).logout(accessToken,refreshToken);
+
+    }
+
+
+}

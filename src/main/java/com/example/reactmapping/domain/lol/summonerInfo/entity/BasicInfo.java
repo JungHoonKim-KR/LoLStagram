8719bd46958c827cba6adb673c7435bc6c6e2cd5
JungHoonKim-KR <kik1232198@gmail.com>
2024-09-28 21:38:52 +0900

    AllArgsConstructor 없애기

--- src/main/java/com/example/reactmapping/domain/lol/summonerInfo/entity/BasicInfo.java
+++ src/main/java/com/example/reactmapping/domain/lol/summonerInfo/entity/BasicInfo.java
@@ -3,13 +3,14 @@ package com.example.reactmapping.domain.lol.summonerInfo.entity;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
+@NoArgsConstructor
+@Builder
 public class BasicInfo {
     @Nullable
     private String leagueId;
@@ -25,5 +26,16 @@ public class BasicInfo {
     private Long totalLosses;
     @Nullable
     private Double totalAvgOfWin;
-}
 
+    // Constructor without @AllArgsConstructor
+    public BasicInfo(@Nullable String leagueId, @Nullable String tier, @Nullable Long tierRank, @Nullable Long leaguePoints
+            , @Nullable Long totalWins, @Nullable Long totalLosses, @Nullable Double totalAvgOfWin) {
+        this.leagueId = leagueId;
+        this.tier = tier;
+        this.tierRank = tierRank;
+        this.leaguePoints = leaguePoints;
+        this.totalWins = totalWins;
+        this.totalLosses = totalLosses;
+        this.totalAvgOfWin = totalAvgOfWin;
+    }
+}

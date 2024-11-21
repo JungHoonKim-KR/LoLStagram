5671ecb992d8e915e6ef0785f8526a8a7cbbc040
Junghoon Kim <kik1232198@gmail.com>
2024-11-21 10:20:03 +0900

    .

--- frontend/src/js/auth/Login.js
+++ frontend/src/js/auth/Login.js
@@ -4,10 +4,10 @@ import {useNavigate} from 'react-router-dom';
 import "../../css/Login.css"
 import google_btn from "../../images/btn_login_google.png"
 //서버로 인증을 요청할 uri (서버의 webSecurityConfig의 base uri와 일치해야 한다)
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const API_BASE_URL ="http://ec2-3-39-119-168.ap-northeast-2.compute.amazonaws.com:8080";
 
 //서버에서 인증을 완료한 후에 프론트엔드로 돌아올 redirect uri (app.oauth2.authorized-redirect-uri와 일치해야 한다)
-export const OAUTH2_REDIRECT_URI = process.env.REACT_APP_OAUTH2_REDIRECT_URI;
+export const OAUTH2_REDIRECT_URI = "http://ec2-3-39-119-168.ap-northeast-2.compute.amazonaws.com:3000/oauth2/redirect";
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorization/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorization/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const NAVER_AUTH_URL = API_BASE_URL + '/oauth2/authorization/naver?redirect_uri=' + OAUTH2_REDIRECT_URI;
